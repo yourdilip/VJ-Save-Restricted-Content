4484854117e39e07a4d4605b3bfd8e90fb53cde4
yourdilip <108778505+yourdilip@users.noreply.github.com>
2024-10-29 14:33:35 +0530

    Update save.py

--- TechVJ/save.py
+++ TechVJ/save.py
@@ -22,44 +22,95 @@ def get(obj, key, default=None):
         return default
 
 
-async def downstatus(client: Client, statusfile, message):
-    while True:
-        if os.path.exists(statusfile):
-            break
-
-        await asyncio.sleep(3)
-      
-    while os.path.exists(statusfile):
-        with open(statusfile, "r") as downread:
-            txt = downread.read()
-        try:
-            await client.edit_message_text(message.chat.id, message.id, f"Downloaded : {txt}")
-            await asyncio.sleep(10)
-        except:
-            await asyncio.sleep(5)
+# download status
+PROGRESS_BAR = """\n╭──💥 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐢𝐧𝐠 💥──╮ 
+├ ▰▰▰▰▰▱▱▱▱▱
+├ 𝗦𝗽𝗲𝗲𝗱 ⚡ ➠ {1}
+├ 𝗣𝗿𝗼𝗴𝗿𝗲𝘀𝘀 🧭 ➠ {0} 
+├ 𝗟𝗼𝗮𝗱𝗲𝗱 🗂️ ➠ {3}/s
+├ 𝗦𝗶𝘇𝗲 🧲 ➠  {2} 
+├ 𝗘𝗧𝗔 ⏳ ➠ {4} 
+╰──── 𝐓𝐇𝐄𝐑𝐌𝐎𝐏𝐎𝐋𝐈™ 🐦‍🔥 ────╯"""
 
 
 # upload status
-async def upstatus(client: Client, statusfile, message):
-    while True:
-        if os.path.exists(statusfile):
-            break
-
-        await asyncio.sleep(3)      
-    while os.path.exists(statusfile):
-        with open(statusfile, "r") as upread:
-            txt = upread.read()
-        try:
-            await client.edit_message_text(message.chat.id, message.id, f"Uploaded : {txt}")
-            await asyncio.sleep(10)
-        except:
-            await asyncio.sleep(5)
+PROGRESS_BAR = """\n╭──💥  𝐔𝐩𝐥𝐨𝐚𝐝𝐢𝐧𝐠 💥──╮ 
+├ ▰▰▰▰▰▱▱▱▱▱
+├ 𝗦𝗽𝗲𝗲𝗱 ⚡ ➠ {1}
+├ 𝗣𝗿𝗼𝗴𝗿𝗲𝘀𝘀 🧭 ➠ {0} 
+├ 𝗟𝗼𝗮𝗱𝗲𝗱 🗂️ ➠ {3}/s
+├ 𝗦𝗶𝘇𝗲 🧲 ➠  {2} 
+├ 𝗘𝗧𝗔 ⏳ ➠ {4} 
+╰──── 𝐓𝐇𝐄𝐑𝐌𝐎𝐏𝐎𝐋𝐈™ 🐦‍🔥 ────╯"""
 
 
 # progress writer
-def progress(current, total, message, type):
-    with open(f'{message.id}{type}status.txt', "w") as fileup:
-        fileup.write(f"{current * 100 / total:.1f}%")
+async def progress_bar(current, total, ud_type, message, start):
+
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        # if round(current / total * 100, 0) % 5 == 0:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+
+        progress = "{0}{1}".format(
+            ''.join(["▰" for i in range(math.floor(percentage / 10))]),
+            ''.join(["▱" for i in range(10 - math.floor(percentage / 10))]))
+            
+        tmp = progress + PROGRESS_BAR.format( 
+            round(percentage, 2),
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),
+            # elapsed_time if elapsed_time != '' else "0 s",
+            estimated_total_time if estimated_total_time != '' else "0 s"
+        )
+        try:
+            await message.edit(
+                text="{}\n\n{}".format(ud_type, tmp),)             
+                
+        except:
+            pass
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2] 
+
+
+
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60      
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
 
 
 # start command
